7d824d75aeac211dc1e5077e7bafccd50eef7cc0
ethanow <owch0002@e.ntu.edu.sg>
2016-02-20 17:20:41 +0800

    Configured Stripe with Purchase item in About

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,22 +1,22 @@
-class ChargesController < Application Controller
+class ChargesController < ApplicationController
+
   def create
-    # Amount in cents
-    @amount = 500
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+  )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => params[:amount],
+    :description => 'Rails Stripe customer',
+    :currency    => 'sgd'
+  )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
 end
